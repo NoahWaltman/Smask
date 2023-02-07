72d6a12fafc43809437aff2a80606f5d5df70c9a
Noah Waltman <noahwaltman123@gmail.com>
2023-02-07 10:49:34 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sklearn.preprocessing as skl_pre
+import sklearn.linear_model as skl_lm
+import sklearn.discriminant_analysis as skl_da
+import sklearn.neighbors as skl_nb
